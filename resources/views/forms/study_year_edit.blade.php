ee9440efae79eeabe0658c657dea35caf56f7df7
Aslangery <Aslangery@gmail.com>
2018-02-07 20:16:37 +0300

    Формы: предметы, учебные года.

---
+++ resources/views/forms/study_year_edit.blade.php
@@ -0,0 +1,25 @@
+<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Учебные года</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="study_year">Учебный год</label>  
+  <div class="col-md-4">
+  <input id="study_year" name="study_year" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="save"></label>
+  <div class="col-md-4">
+    <button id="save" name="save" class="btn btn-default">Сохранить</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
