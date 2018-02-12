efb0e13f989fb438ea92a460cabfb556d9f463fe
Aslangery <Aslangery@gmail.com>
2018-02-13 01:01:39 +0300

    views editing

--- resources/views/lists/subject_list.blade.php
+++ resources/views/lists/subject_list.blade.php
@@ -6,6 +6,7 @@
         </div>
         <div class="panel-body">
             <div class="list-group">
+                <form id="edit" action="" method="post">
                 <table class="table table-bordered">
                     <tr>
                         <th>#</th>
@@ -18,10 +19,10 @@
                             <td><a href="{{url('/subjects/'.$sub->id.'/edit')}}">{{$sub->title}}</a></td>
                             <td>
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-success btn-xs">
+                                    <button id="edit{{$sub->id}}" name="{{$sub->id}}" type="button" class="btn btn-success btn-xs">
                                         <span class="glyphicon glyphicon-edit"></span>
                                     </button>
-                                    <button type="button" class="btn btn-danger btn-xs">
+                                    <button id="delete{{$sub->id}}" name="{{$sub->id}}" type="button" class="btn btn-danger btn-xs">
                                         <span class="glyphicon glyphicon-remove"></span>
                                     </button>
                                 </div>
@@ -30,8 +31,25 @@
 
                     @endforeach
                 </table>
+                    {{csrf_field()}}
+                    {{method_field('DELETE')}}
+                </form>
             </div>
 
         </div>
     </div>
+
+@endsection
+@section ('script')
+    <script type="text/javascript">
+        jQuery("button[id^='delete']").click(function(){
+            jQuery("#edit").attr("action","/subjects/"+this.name);
+            jQuery("#edit").submit();
+        });
+        jQuery("button[id^='edit']").click(function(){
+            jQuery("#edit").attr("action","/subjects/"+this.name+'/edit');
+            jQuery("input[name='_method']").val('get');
+            jQuery("#edit").submit();
+        });
+    </script>
 @endsection
